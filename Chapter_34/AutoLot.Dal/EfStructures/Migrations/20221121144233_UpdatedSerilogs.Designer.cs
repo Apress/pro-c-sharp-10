7706b27f22ed751e51d397cbc56e83f99092d4e2
Octopon <devnewgame@gmail.com>
2022-11-21 16:44:19 +0200

    Update columns lenght(schema: Logging, table: SeriLogs)

--- Chapter_34/AutoLot.Dal/EfStructures/Migrations/20221121144233_UpdatedSerilogs.Designer.cs
+++ Chapter_34/AutoLot.Dal/EfStructures/Migrations/20221121144233_UpdatedSerilogs.Designer.cs
@@ -4,6 +4,7 @@ using AutoLot.Dal.EfStructures;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AutoLot.Dal.EfStructures.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221121144233_UpdatedSerilogs")]
+    partial class UpdatedSerilogs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
